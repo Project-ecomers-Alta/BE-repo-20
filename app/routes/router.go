91343cebdeb1408d6fcb216bc3a6e805eb3bc229
Hanapibagas <hanafibagas4@gmail.com>
2024-01-16 16:12:57 +0800

    setup struktur

---
+++ app/routes/router.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+func InitRuter(db *gorm.DB, e *echo.Echo) {
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"BE-REPO-20/app/configs"
+	"BE-REPO-20/app/database"
+	"BE-REPO-20/app/routes"
 
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	cfg := configs.InitConfig()
+	dbsql := database.InitDBMysql(cfg)
+	database.InitMigrate(dbsql)
+
+	e := echo.New()
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.CORS())
+	e.Use(middleware.Logger())
+
+	routes.InitRuter(dbsql, e)
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
