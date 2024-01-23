9693185eca99cf0bf177f1d8f2e5d51940de817d
Achmad Yoga Prananda <achmd.yoga@gmail.com>
2024-01-24 00:31:49 +0700

    add feature Select Product by id product

--- features/product/handler/handler_product.go
+++ features/product/handler/handler_product.go
@@ -3,7 +3,9 @@ package handler
 import (
 	"BE-REPO-20/features/product"
 	"BE-REPO-20/utils/responses"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -27,4 +29,20 @@ func (handler *ProductHandler) SelectAllProduct(c echo.Context) error {
 	productsResponse := CoreToResponseList(products)
 
 	return c.JSON(http.StatusOK, responses.WebResponse("success read products.", productsResponse))
+}
+
+func (handler *ProductHandler) SelectProductById(c echo.Context) error {
+	id := c.Param("product_id")
+	idParam, errConv := strconv.Atoi(id)
+	if errConv != nil {
+		return c.JSON(http.StatusBadRequest, responses.WebResponse("error. id should be number", nil))
+	}
+	product, err := handler.productService.SelectProductById(1, idParam)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, responses.WebResponse("error read data. "+err.Error(), nil))
+	}
+	productResponse := CoreToResponse(*product)
+	fmt.Println(productResponse)
+
+	return c.JSON(http.StatusOK, responses.WebResponse("success read product.", productResponse))
 }
